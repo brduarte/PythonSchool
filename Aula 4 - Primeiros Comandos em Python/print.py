0cbaf6a8b33e562c8d5df14d73f443c5127d5899
brduarte <brunomacielduarte32@gmail.com>
2021-03-20 23:55:59 -0300

    Update print.py

--- Aula 4 - Primeiros Comandos em Python/print.py
+++ Aula 4 - Primeiros Comandos em Python/print.py
@@ -1,1 +1,15 @@
+# Imprimindo string
 print('Hello World!')
+# Imprimindo operações
+print(3+5)
+# Imprimindo concatenação
+print('7'+'4')
+print('Valor do número', 10)
+
+## Variáveis
+
+nome = 'Bruno Duarte'
+idade = 25
+peso = 56
+
+print(nome,idade,peso)
